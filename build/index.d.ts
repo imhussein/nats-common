5f9e5f5d3806f6d818971e33d5c23526b434f1c6
mh10445 <hussein@blockgemini.com>
2020-08-16 15:50:02 +0400

    Updates

---
+++ build/index.d.ts
@@ -0,0 +1,5 @@
+interface Color {
+    red: number;
+}
+declare const color: Color;
+export { color };
---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.color = void 0;
+var color = {
+    red: 10,
+};
+exports.color = color;
+console.log(color);
