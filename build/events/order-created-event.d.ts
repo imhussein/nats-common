4dfa22af42a9496c7ac3c7bfba093fe5f883ceb1
mh10445 <hussein@blockgemini.com>
2020-08-20 16:50:24 +0400

    Add

--- build/events/order-created-event.d.ts
+++ build/events/order-created-event.d.ts
@@ -4,7 +4,7 @@ export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated;
     data: {
         id: string;
-        status: OrderStatus.Created;
+        status: OrderStatus;
         userId: string;
         expiresAt: string;
         ticket: {
